18d2b3b4517d98fab20377fd69bcfd0f79dbb7a3
codezerro <codezerro@users.noreply.github.com>
2024-07-29 19:58:58 +0000

    deploy: b16c31e3d20978f7e8459ab3e8083019b3b59e2c

---
+++ assets/NavWrapper-5EAK07zq.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Navbar-Bj4M-BD4.js","assets/index-ozZgyf-N.js","assets/index-DvdnpXeE.css","assets/icon-Caz-k3G0.js","assets/index-C1Rc6CcN.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{R as e,_ as t,j as s,s as a}from"./index-ozZgyf-N.js";const r=e.lazy(()=>t(()=>import("./Navbar-Bj4M-BD4.js"),__vite__mapDeps([0,1,2,3,4]))),l=()=>s.jsx("div",{className:`${a.paddingX} ${a.flexCenter} fixed  w-full bg-slate-800 z-50`,children:s.jsx("div",{className:`${a.boxWidth} `,children:s.jsx(r,{})})});export{l as default};
