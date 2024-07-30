cc85eaca19ef92461ae84f6a98036d0d543a3e0e
codezerro <codezerro@users.noreply.github.com>
2024-07-30 09:23:56 +0000

    deploy: 926ef9d1cb06de83d24dcbd69f125fd320ab36f9

---
+++ assets/NavWrapper-DTjKICdG.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Navbar-CSqGWuXa.js","assets/index-DNfl3feq.js","assets/index-C13wLs9B.css","assets/icon-Caz-k3G0.js","assets/index-C1Rc6CcN.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{R as e,_ as t,j as s,s as a}from"./index-DNfl3feq.js";const r=e.lazy(()=>t(()=>import("./Navbar-CSqGWuXa.js"),__vite__mapDeps([0,1,2,3,4]))),l=()=>s.jsx("div",{className:`${a.paddingX} ${a.flexCenter} fixed  w-full bg-slate-800 z-50`,children:s.jsx("div",{className:`${a.boxWidth} `,children:s.jsx(r,{})})});export{l as default};
